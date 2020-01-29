1e60a45db99e92b641721349d3bf484d5745b136
ovidiu todea <ovi.todea@yahoo.com>
2020-01-29 15:58:18 +0200

    Calc2

---
+++ src/main/Calc2.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Calc2 {
+
+	public static void main(String[] args) {
+		int a=3;
+		if(++a < 4)
+			{if (a++ < 4)
+				System.out.println(a);
+			else
+				System.out.println(a);}
+		else System.out.println("test");
+
+	}
+
+}
